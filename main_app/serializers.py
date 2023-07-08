e7f3e15a27392394aa7fc79cbed72f71c705ac2f
larrytu327 <larrytu327@gmail.com>
2023-07-08 15:13:46 -0700

    completed setting up API

---
+++ main_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('type', 'password', 'username', 'first_name', 'last_name', 'email', 'phone', 'address_line1', 'address_city', 'address_state', 'address_zip', 'birthdate', 'info', 'parents', 'teachers', 'students', 'classes', 'completed')
